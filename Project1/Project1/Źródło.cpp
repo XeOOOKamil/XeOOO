a2ed26fcdb2196538ea5ea1845e0fa059011d41e
XDDDD <>
2019-05-14 13:48:50 +0200

    dodano nowy projekt z skryptem

--- Project1/Project1/Źródło.cpp
+++ Project1/Project1/Źródło.cpp
@@ -1,1 +1,29 @@
-vhbghfghfghfg
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//dwie funkcje - przezWartosc i przezReferencje,przez Wartosc(dwa parametry formalne a i b i typie int,przezReferencje dwa parametry formalne o nazwie x i y i typie float, dwie funckje wykonujace mnozenie,mamy wyswietlic wartosci zmiennych po wyniku
+
+
+
+
+
+int przezWartosc(int a, int b) {
+	a += 1;
+	
+	b += 1;
+
+		return a * b;
+}
+
+int main() {
+	int a, b;
+	cout << "Podaj a:";
+	cin >> a;
+	cout << "Podaj b:";
+	cin >> b;
+
+	cout<<przezWartosc(a,b);
+	system("pause");
+	return 0;
+}
---
+++ Project1/Project1/Źródło1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char liczba[20];
+
+
+void pio ()
+{ 
+
+
+	    cout << "Wprowadz wyraz a ja wyswietle pierwsza i ostatnia litere";
+		cin >> liczba;
+		cout << "Pierwsza litera to" << nazwa[0];
+		cout << "Ostatnia litera to";
+
+
+
+	
+}
+int main()
+{
+	pio();
+	    system("pause");
+		return 0;
+
+
+
+}
