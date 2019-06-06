92d5cc0bcd1d8b68844fc4425b0fd94c203aff29
Kamil Kubaszek <ObelixKJ@o2.pl>
2019-06-06 19:07:23 +0100

    cyk

---
+++ cwiczenie5strona63/cwiczenie5strona63/Źródło.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string tekst;
+	int liczba_sp = 0;
+	cout << "Podaj swoj tekst: ";
+	getline(cin, tekst);
+	for (int i = 0; i < tekst.length(); i++)
+	{
+		if (tekst[i] == ' ')
+			liczba_sp++;
+	}
+	cout << "Ilosc spacji: " << liczba_sp << endl;
+
+	system("pause");
+}
